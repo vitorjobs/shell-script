1845402abb46e163399cea3228b76d060186c037
vitorjobs <vittorjobs@gmail.com>
2022-12-15 14:15:07 -0300

    Boas Práticas

---
+++ Exercicios/Mod02/01_BoasPraticas.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ #---------------------------------------------------------------#
+ echo "Aula: 10"
+ echo "Boas Práticas"
+
+# Cabeçalho:
+ # - Nome
+ # - Mantenedor
+ # - Exemplo - (Execuções)
+ # - Versionamento
+ # - Testado - Ambientes que os Script foram testados
+
+# Estrutura:
+# Comentários:
+# Estruturação do Código:
+
