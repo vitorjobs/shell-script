04de229835e0066a316378f1f44bc72e1122afdf
vitorjobs <vittorjobs@gmail.com>
2022-12-16 13:25:19 -0300

    Debug

---
+++ Exercicios/Mod04/debug.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ #---------------------------------------------------------------#
+ echo "Aula: 21"
+ echo "Debug no Shell Script com :  bash -x ./debug_1.sh -m
+  Mostra o relatório geral (DUMP), instâncias, valores.
+ "
+
+echo "Debug no Shell Script com `set -x` 
+  Alocado dentro de uma parte específica do script (com abertura e fechamento)
+ "
+
+echo "Debug no Shell Script com `function` 
+  Escrever uma função de dbug 
+ "
+
+  #---------------------------------------------------------------#
+ echo "Aula: 21"
+ echo "Debug no Shell Script com - -x -v 
+  Mostra o relatório geral (DUMP), instâncias, valores.
+ "
